50e682d9e716a4c5b69c61b6d987ba094b4eb9c9
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-04-24 21:36:20 +0500

    Задание начального положения рокетки

--- racket.py
+++ racket.py
@@ -1,7 +1,8 @@
 """Модуль настроек отображения и поведения рокетки."""
 
 from tkinter import Canvas
-from constants import LENGHT_RACKET, HEIGHT_RACKET, X_0_RACKET, Y_0_RACKET
+from constants import (LENGHT_RACKET, HEIGHT_RACKET, X_0_RACKET, Y_0_RACKET,
+                       WINDOW_HEIGHT, WINDOW_WIDTH)
 
 
 class Racket:
@@ -10,3 +11,6 @@ class Racket:
         self.id = canvas.create_rectangle(X_0_RACKET, Y_0_RACKET,
                                           LENGHT_RACKET, HEIGHT_RACKET,
                                           fill='red')
+        self.canvas.move(self.id, WINDOW_WIDTH // 2 - LENGHT_RACKET // 2,
+                         WINDOW_HEIGHT - 50)
+        self.x = 0
