32b8261cdda53afc8e5fb4c2e686ceacf5c69fd0
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-04-24 21:12:20 +0500

    Поведение шарика при попадании на рокетку

--- ball.py
+++ ball.py
@@ -7,9 +7,9 @@ from constants import (RADIUS_X, RADIUS_Y, X_0_BALL, Y_0_BALL,
 
 
 class Ball:
-    def __init__(self, canvas: Canvas, platform, score) -> None:
+    def __init__(self, canvas: Canvas, racket, score) -> None:
         self.canvas = canvas
-        self.platform = platform
+        self.racket = racket
         self.score = score
         self.id = canvas.create_oval(X_0_BALL, Y_0_BALL,
                                      RADIUS_X, RADIUS_Y,
@@ -27,6 +27,18 @@ class Ball:
             return
         if positions.y_up_left <= 0:
             self.y = 2
+        if self.hit_the_racket(positions):
+            self.score.add()
+            self.y = -2
+
+    def hit_the_racket(self, positions: 'Position') -> bool:
+        racket_position = Position(*self.canvas.coords(self.racket.id))
+        return (
+            positions.x_down_right >= racket_position.x_up_left and
+            positions.x_up_left <= racket_position.x_down_right and
+            racket_position.y_up_left <= positions.y_down_right <=
+            racket_position.y_down_right
+        )
 
 
 @dataclass
